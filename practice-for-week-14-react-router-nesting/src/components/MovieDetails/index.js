ddb7bda2ead2074e04f5ba4ffe188c1b41321fe2
George Merida <gmerida92@gmail.com>
2022-08-31 22:03:24 -0700

    50% done

--- practice-for-week-14-react-router-nesting/src/components/MovieDetails/index.js
+++ practice-for-week-14-react-router-nesting/src/components/MovieDetails/index.js
@@ -1,9 +1,12 @@
+import { NavLink, Route, Switch, useParams } from "react-router-dom";
+
 function MovieDetails() {
   return (
     <div className='comp purple'>
       <h1>MovieDetails Component</h1>
     </div>
   );
-}
+
+};
 
 export default MovieDetails;
--- practice-for-week-14-react-router-nesting/src/components/Movies/index.js
+++ practice-for-week-14-react-router-nesting/src/components/Movies/index.js
@@ -1,7 +1,22 @@
-function Movies() {
+import { NavLink, Route, Switch, useParams } from "react-router-dom";
+import MovieDetails from '../MovieDetails'
+
+function Movies({movies}) {
   return (
     <div className='comp orange'>
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map((movieObj) => {
+          return (
+            <NavLink to={`/movies/${movieObj.id}`}>{movieObj.title}</NavLink>
+          )
+        })}
+      </nav>
+      <Switch>
+        <Route path="/movies/:movieId">
+          <MovieDetails />
+        </Route>
+      </Switch>
     </div>
   );
 }
